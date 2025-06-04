7d8c279d68a052d107f33519fdd996d2ca2d094c
PlayerEG <enderg@qq.com>
2025-06-04 17:05:10 +0800

    feat(h1): 添加轮播图功能并优化页面样式
    - 在 h1.css 中添加了轮播图的样式，包括左右按钮和背景图片
    - 修改了 public.css 中的背景图片样式，使其固定并覆盖整个页面
    - 更新了 h1.html 的结构，增加了轮播图相关的 HTML 元素
    - 重构了 h1.js，实现了轮播图的自动播放和手动切换功能

--- JS/h1.js
+++ JS/h1.js
@@ -10,15 +10,16 @@ window.onload = function () {
     request = null;
 
     let img = document.getElementById('one_by_one_img');
-    // 获取id为show-box-right的所有元素
-    let right = document.getElementById('show-box-right');
+    let left_box = document.getElementById('show-box-left');
+    let left_btn = document.getElementById('switch-left-btn');
+    let right_btn = document.getElementById('switch-right-btn');
 
-    let index = 1; // 索引
-    console.log(img);
+    let index = 0; // 索引
+    let timer = null; // 定时器
 
     // 播放第一张图片
     img.src = data[0].imgurl;
-    right.innerHTML = `
+    left_box.innerHTML = `
         <div><span>电影名称：</span>${data[0].电影名称}</div>
         <div><span>标签：</span>${data[0].标签}</div>
         <div><span>上映日期：</span>${data[0].上映日期}</div>
@@ -27,10 +28,69 @@ window.onload = function () {
         <div><span>导演：</span>${data[0].导演}</div>
         <div><span>编剧：</span>${data[0].编剧}</div>
         `;
-    setInterval(play, 3000);
+    // 自动播放
+    autoPlay();
+
+    // 鼠标移入移出暂停播放
+    img.onmouseover = function () {
+        clearInterval(timer);
+    }
+    img.onmouseout = function () {
+        autoPlay();
+    }
+    left_box.onmouseover = function () {
+        clearInterval(timer);
+    }
+    left_box.onmouseout = function () {
+        autoPlay();
+    }
+    left_btn.onmouseover = function () {
+        clearInterval(timer);
+    }
+    left_btn.onmouseout = function () {
+        autoPlay();
+    }
+    right_btn.onmouseover = function () {
+        clearInterval(timer);
+    }
+    left_btn.onmouseout = function () {
+        autoPlay();
+    }
+
+
+    // 点击左右按钮切换图片
+    left_btn.onclick = function () {
+        clearInterval(timer);
+        index--;
+        if (index < 0) {
+            index = data.length - 1;
+        }
+        play();
+    }
+    right_btn.onclick = function () {
+        clearInterval(timer);
+        index++;
+        if (index > data.length - 1) {
+            index = 0;
+        }
+        play();
+    }
+
+    // 自动播放方法
+    function autoPlay() {
+        timer = setInterval(
+            function () {
+                index > data.length - 2 ? index = 0 : index++;
+                play();
+                console.log(index);
+            }
+            , 3000);
+    }
+
+    // 切换内容方法
     function play() {
         img.src = data[index].imgurl;
-        right.innerHTML = `
+        left_box.innerHTML = `
         <div><span>电影名称：</span>${data[index].电影名称}</div>
         <div><span>标签：</span>${data[index].标签}</div>
         <div><span>上映日期：</span>${data[index].上映日期}</div>
@@ -39,9 +99,6 @@ window.onload = function () {
         <div><span>导演：</span>${data[index].导演}</div>
         <div><span>编剧：</span>${data[index].编剧}</div>
         `;
-        index = (index + 1) % data.length;
-        console.log(index);
-        console.log(img.src);
     }
 
 };
